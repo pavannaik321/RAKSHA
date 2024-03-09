1aaa3a8418ce699edc298b67c6331c4112b9e4b6
pavan naik <pavannaik@pavans-MacBook-Air.local>
2024-03-09 15:44:47 +0530

    added backend template code

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,26 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 8000;
 
-app.use("/",(req,res)=>{
-    res.json({message:"Hello From Express App"});
-})
+app.use(cors(
+    {
+        origin:['https://raksha-frontend.vercel.app'],
+        methods:["POST","GET"],
+        credentials:true
+    }
+))
+app.use(express.json())
 
-app.listen(port,()=>{
-    console.log("starting ")
+app.use("/", (req, res) => {
+  res.json({ message: "Hello From Express App" });
+});
+
+app.get("/check",(req,res)=>{
+    res.json({message:"connection established"})
 })
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
