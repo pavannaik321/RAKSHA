8460876629dca524c778deb9f8a03e2c280479f7
pavan naik <pavannaik@pavans-MacBook-Air.local>
2024-03-10 00:11:11 +0530

    added data

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
-import Logo from "../assets/logo.png"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center bg-gray-800 text-white h-16">
-      <img
-        src={Logo}
-        alt="Raksha Logo"
-        className="h-8 w-auto"
-      />
-      <span className="ml-2 text-lg font-semibold">Raksha</span>
+      <Link to="/" className="flex items-center">
+        {/* <img
+          src="/path_to_your_logo.png"
+          alt="Raksha Logo"
+          className="h-8 w-auto"
+        /> */}
+        <span className="ml-2 text-lg font-semibold">Raksha</span>
+      </Link>
     </div>
   );
 };
