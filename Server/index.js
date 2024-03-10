50bfabc284b0b5d7e7d7fac7bdc5b435f36607ab
pavan naik <pavannaik@pavans-MacBook-Air.local>
2024-03-10 12:50:17 +0530

    socket start

--- Server/index.js
+++ Server/index.js
@@ -1,39 +1,39 @@
 const express = require('express');
-const http = require('http');
+
 const cors = require('cors');
 const connection = require("./db");
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
-const socketIo = require('socket.io');
-
+const {Server} = require('socket.io')
 const app = express();
 const port = 8000;
+const port2 = 8001;
+const io = new Server();
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  methods: ["POST", "GET", "PUT"],
-  credentials: true
-}));
+app.use(cors(
+    {
+        origin:['http://localhost:5173'],
+        methods:["POST","GET","PUT"],
+        credentials:true
+    }
+))
 
 // database connection
 connection();
 
-app.use(express.json());
+app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-
+console.log("1")
 app.use("/", require("./routes/authRouts"));
 
-// Create a single HTTP server for both Express and socket.io
-const server = http.createServer(app);
 
-// Initialize socket.io with the server
-const io = socketIo(server);
-
-// Socket.io logic goes here
+// signanling code
+io.on("connection",(socket)=>{});
 
 
-server.listen(port, () => {
+// Start the server
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
+io.listen(port2)
